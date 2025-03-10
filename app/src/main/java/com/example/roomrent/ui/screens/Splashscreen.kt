426bfae328043a4e38fc8ac836807728584ecb56
Vikash Kumar Chaurasiya <vikashchaurasiya2078@gmail.com>
2025-03-11 02:53:39 +0530

    updated

--- app/src/main/java/com/example/roomrent/ui/screens/Splashscreen.kt
+++ app/src/main/java/com/example/roomrent/ui/screens/Splashscreen.kt
@@ -3,7 +3,11 @@ package com.example.roomrent.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -12,7 +16,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.roomrent.R
 import kotlinx.coroutines.delay
@@ -25,11 +33,10 @@ fun Splashscreen(navController: NavController) {
         delay(300)
         visible = true
         delay(2000)
-        navController.navigate("Dashboard"){
+        navController.navigate("Dashboard") {
             popUpTo("splash_screen") { inclusive = true }
         }
     }
-
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -39,6 +46,17 @@ fun Splashscreen(navController: NavController) {
         Image(
             painter = painterResource(id = R.drawable.splashscreen_logo),
             contentDescription = "splashscreen"
+        )
+
+
+        Spacer(modifier = Modifier.height(300.dp))
+
+        Text(
+            text = "Developed by Vikash Kumar Chaurasiya",
+            style = TextStyle(color = Color.Black, fontSize = 16.sp), // Adjust style as needed
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.CenterHorizontally)
         )
     }
 }
